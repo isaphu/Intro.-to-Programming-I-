b45ad2bdf810b4770e20a8a5fe55265359dfc829
uronohan <uronohan@gmail.com>
2020-10-15 14:17:09 +0700

    adding hack a robot folder

---
+++ Sketch/sketch.js
@@ -0,0 +1,49 @@
+function setup()
+{
+    createCanvas(1350,600);
+}
+
+function draw()
+{
+    
+    // //this command clears the background to black (try changing the numbers)
+    // background(0,0,0);
+    
+    // //change the fill color
+    // fill(255,0,0);
+    
+    // //change the outline color
+    // stroke(0,255,0);
+    
+    // //this command draws a rectangle with the outline and fill colors that we just set
+    // rect(125,125,250,250);
+    // integer RGBA notation
+    background('rgba(0,255,0, 0.25)');
+
+
+    rect(50,50,80,80);
+    rect(50,150,80,80);
+    rect(50,250,80,80);
+    rect(50,350,80,80);
+    rect(50,450,80,80);
+
+    rect(150,50,80,80);
+    rect(150,150,80,80);
+    rect(150,250,80,80);
+    rect(150,350,80,80);
+    rect(150,450,80,80);
+
+    rect(250,50,80,80);
+    rect(250,150,80,80);
+    rect(250,250,80,80);
+    rect(250,350,80,80);
+    rect(250,450,80,80);
+
+    rect(350,50,80,80);
+    rect(350,150,80,80);
+    rect(350,250,80,80);
+    rect(350,350,80,80);
+    rect(350,450,80,80);
+    
+}
+
