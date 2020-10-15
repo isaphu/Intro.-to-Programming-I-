96f447d054f54d40d3d4cae070981f132384b576
uronohan <uronohan@gmail.com>
2020-10-15 13:52:09 +0700

    adding more sketches

--- sketch.js
+++ sketch.js
@@ -1,30 +1,31 @@
-//This is a simple template for you to experiment with
-
-//This is a comment  ... the two slashes at the start mean that the computer ignores it
-
-//You can look up more commands here ... https://p5js.org/reference/
-
 function setup()
 {
-    //this function runs once at the start of your program
-    
-    //this command creates a drawing area in the browser - it should only be called once
-    createCanvas(500,500);
+    createCanvas(1350,600);
 }
 
 function draw()
 {
     
-    //this command clears the background to black (try changing the numbers)
-    background(0,0,0);
+    // //this command clears the background to black (try changing the numbers)
+    // background(0,0,0);
     
-    //change the fill color
-    fill(255,0,0);
+    // //change the fill color
+    // fill(255,0,0);
     
-    //change the outline color
-    stroke(0,255,0);
+    // //change the outline color
+    // stroke(0,255,0);
     
-    //this command draws a rectangle with the outline and fill colors that we just set
-    rect(125,125,250,250);
+    // //this command draws a rectangle with the outline and fill colors that we just set
+    // rect(125,125,250,250);
+    // integer RGBA notation
+    background('rgba(0,255,0, 0.25)');
+
+
+    rect(50,50,80,80);
+    rect(50,150,80,80);
+    rect(50,250,80,80);
+    rect(50,350,80,80);
+    rect(50,450,80,80);
     
 }
+
